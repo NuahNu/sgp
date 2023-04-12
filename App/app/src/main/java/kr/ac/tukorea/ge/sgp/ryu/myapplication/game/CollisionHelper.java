b16099a61bd85a4dfe465a2c0153e41820da4899
RYUYW <yyw980312@gmail.com>
2023-04-13 00:13:50 +0900

    Update CollisionHelper.java
    버그 해결?

--- App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/CollisionHelper.java
+++ App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/CollisionHelper.java
@@ -24,30 +24,42 @@ public class CollisionHelper {
         RectF r1 = obj1.getCollisionRect();
         RectF r2 = obj2.getCollisionRect();
 
+        float r1_h = (r1.top - r1.bottom)/2;
+        float r1_w = (r1.right - r1.left)/2;
+        float r2_h = (r2.top - r2.bottom)/2;
+        float r2_w = (r2.right - r2.left)/2;
+
         Vector2D dist = new Vector2D(Math.abs((r1.left+r1.right - r2.left-r2.right)/2), Math.abs((r1.top+r1.bottom - r2.top-r2.bottom)/2));
         Vector2D vec[] = new Vector2D[4];
         float radian;
         radian = obj1.getRadian();
 
         vec[0] = new Vector2D(Math.cos(radian), Math.sin(radian));
+        vec[0].multiply(r1_w);
         vec[1] = new Vector2D(Math.cos(radian+Math.PI/2), Math.sin(radian+Math.PI/2));
+        vec[1].multiply(r1_h);
 
         radian = obj2.getRadian();
 
         vec[2] = new Vector2D(Math.cos(radian), Math.sin(radian));
+        vec[2].multiply(r2_w);
         vec[3] = new Vector2D(Math.cos(radian+Math.PI/2), Math.sin(radian+Math.PI/2));
+        vec[3].multiply(r2_h);
+
 
         Vector2D unit;
         for(int i=0; i<4; i++){
             double sum = 0;
             unit = vec[i].getNormalized();
             for(int j=0; j<4; j++){
-                sum += Math.abs(Vector2D.dot(vec[j], unit));
+                double tmp = Vector2D.dot(vec[j], unit);
+                sum += Math.abs(tmp);
             }
             if(Math.abs(Vector2D.dot(dist, unit)) > sum){
                 return false;
             }
         }
+
         return true;
     }
 
