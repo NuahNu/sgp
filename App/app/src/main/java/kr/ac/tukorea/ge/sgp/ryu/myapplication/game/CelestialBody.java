bb3c37bd506d03c72563eb91e70fe7c31009bc0b
RYUYW <yyw980312@gmail.com>
2023-04-17 00:27:34 +0900

    Create CelestialBody.java
    중립 오브젝트를 위한 class

---
+++ App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/CelestialBody.java
@@ -0,0 +1,9 @@
+package kr.ac.tukorea.ge.sgp.ryu.myapplication.game;
+
+import kr.ac.tukorea.ge.sgp.ryu.myapplication.framework.Sprite;
+
+public class CelestialBody extends Sprite {
+    public CelestialBody(int bitmapResId, float cx, float cy, float width, float height) {
+        super(bitmapResId, cx, cy, width, height);
+    }
+}
