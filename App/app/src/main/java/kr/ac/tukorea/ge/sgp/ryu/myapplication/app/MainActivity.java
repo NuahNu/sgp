e3c8ecfacc4eff1abde289f8918ed8588f8043e7
RYUYW <yyw980312@gmail.com>
2023-04-17 02:41:12 +0900

    Update MainActivity.java
    import 정리

--- App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/app/MainActivity.java
+++ App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/app/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import kr.ac.tukorea.ge.sgp.ryu.myapplication.R;
 import kr.ac.tukorea.ge.sgp.ryu.myapplication.framework.GameView;
 import kr.ac.tukorea.ge.sgp.ryu.myapplication.game.MainScene;
 
