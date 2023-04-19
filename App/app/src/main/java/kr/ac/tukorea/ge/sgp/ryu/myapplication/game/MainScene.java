d6aacf6512ea4e00cdd509a6a3d588ce76ba07cf
RYUYW <yyw980312@gmail.com>
2023-04-20 03:04:55 +0900

    Update Todo
    주석 참고

--- App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/MainScene.java
+++ App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/MainScene.java
@@ -12,13 +12,29 @@ public class MainScene extends BaseScene {
     public enum Layer {
         bg1, bullet, CelestialBody, ship, bg2, ui, controller, COUNT
     }
+//    bg1
+//      우주배경
+//    bg2
+//      안개
+//    ui
+//      움직임 스틱, 무기 발사.         // 전력시스템
+//    cnotroller
+//      playerDate? 이것도 여기 넣도록 개조.
+//          1. 조종중인 비행기가 죽으면 목록에서 다음 비행기를 가져오도록.
+//          2. 다 죽으면 game over
+//      AIcontroller
+//            AI 조종을 이걸로.
 
     public MainScene(){
         initLayers(Layer.COUNT);
 
+        //---- 이 작업을 controller의 playerData가 하게 만든다?
+        // playerData는 모든 Scene에서 접근 가능해야함.
+        // BaseScene에 있는게 맞지 않을까?
         ArrayList<Ship> shipList = playerData.getShips();
         Ship tmp = shipList.get(0);
         add(Layer.ship, tmp);
+        //----
         add(Layer.controller, new CollisionChecker());
     }
 
@@ -27,6 +43,10 @@ public class MainScene extends BaseScene {
         switch (action) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_MOVE:
+//                스틱을 터치해서 스틱의 각을 정한다.
+//                스틱과 플레이어의 radian이 같으면 가속을 한다.
+//                다르면 플레이어의 각을 스틱의 각으로 회전한다.
+//                무기별 발사 버튼을 눌러 탄 발사.
 //                float x = Metrics.toGameX(event.getX());
 //                float y = Metrics.toGameY(event.getY());
 //                fighter.setTargetPosition(x, y);
--- Todo.md
+++ Todo.md
@@ -1,14 +1,14 @@
 잘 먹고 잘 자기
 README 갱신
-class Weapon 작성
-    그리는 위치 구하기 귀찮더
-    다양한 상태를 그려내기 위해 AnimSprite 를 멤버변수로 갖는다?
-    => 4/19수업에서 사용한 방법을 적용
 class projectile 작성
     PhysicalObject와의 충돌을 위해 HP 관련 변수와 메소드추가.
     Ship의 경우 shield 도 있을듯.
 
 휴대폰 비율과 관계 없이 일정한 크기로 그리게 하려면?
+
+ship 의 파괴시 기능 구현.
+
+
 
 Map 구현?
 향상된 for문 index 사용법?
