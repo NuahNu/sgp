fd75f86ed10f91f02c55c5a3ea1fc831a2e33b4f
RYUYW <yyw980312@gmail.com>
2023-04-20 00:53:16 +0900

    update Weapon etc
    무기 발사 애니메이션 추가

--- App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/Weapon.java
+++ App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/Weapon.java
@@ -3,6 +3,7 @@ package kr.ac.tukorea.ge.sgp.ryu.myapplication.game;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 
+import kr.ac.tukorea.ge.sgp.ryu.myapplication.framework.BaseScene;
 import kr.ac.tukorea.ge.sgp.ryu.myapplication.framework.Sprite;
 
 public class Weapon extends Sprite{
@@ -13,10 +14,15 @@ public class Weapon extends Sprite{
     protected int projectileType; // 0 = 물리, 1 = 에너지 enum?
     protected static Rect srcRect = new Rect();
 
-    // 연사력
-        // 재장전시간
-        // 마지막 발사 시간
-        // 
+    protected boolean isFiring;
+    protected float coolTime;           // 무기의 쿨타임
+    protected float remainingTime;      // 남은 쿨타임
+    protected float firingTime;       // 탄 발사시간.
+    protected int reloadingSprite;    // 재장전 스프라이트 수
+    protected int firingSprite;       // 발사 스프라이트 수
+
+    protected Rect[] rects = null;      // 그릴 이미지의 위치.
+
     // 데미지
     // 사거리
     // 투사체 옵션
@@ -31,6 +37,8 @@ public class Weapon extends Sprite{
         mass = 0;
         //-----------------------
         this.owner = inputOwner;
+        isFiring = false;
+        remainingTime = 0;
     }
 
     public float getMass(){
@@ -38,8 +46,14 @@ public class Weapon extends Sprite{
     }
     
     public void fire(){
-        // update한 위치에 발사체를 생성한다.
-        // 발사체 생성 위치를 따로 정해지 않았다면 이상하게 보일것.
+        if(!isFiring) {
+            if(remainingTime == coolTime){
+                isFiring = true;
+                remainingTime = 0;
+                // update한 위치에 발사체를 생성한다.
+                // 발사체 생성 위치를 따로 정해지 않았다면 이상하게 보일것.
+            }
+        }
     }
 
     public void draw(Canvas canvas, Vector2D weaponLocation) {
@@ -54,12 +68,53 @@ public class Weapon extends Sprite{
     }
 
     public void update() {
+        checkCoolTime();
+        selectRect();
         // owner의 위치에 weaponLocation을 더해 자신의 위치 갱신.?
         x = owner.getX();
         y = owner.getY();
         fixDstRect();
         // 발사체를 생성하는 위치도 여기서 계사능ㄹ 할까?
     }
+
+    private void selectRect() {
+        int rectIndex;
+        if(isFiring){
+            rectIndex = (int) ((remainingTime / firingTime) * (firingSprite)) + (reloadingSprite);
+        }
+        else{
+            rectIndex = (int) ((remainingTime / coolTime) * (reloadingSprite+1));
+        }
+//        if(!isRightSide){
+//            rectIndex = 11 - rectIndex;
+//        }
+
+        srcRect = rects[rectIndex];
+    }
+
+    private void checkCoolTime() {
+        if(isFiring){
+            if (remainingTime < firingTime) {
+                remainingTime += BaseScene.frameTime;
+                if(remainingTime > firingTime) {
+                    isFiring = false;
+                    remainingTime = 0;
+                }
+            }
+        }
+        else
+            if(remainingTime < coolTime) {
+                remainingTime += BaseScene.frameTime;
+                if(remainingTime > coolTime)
+                {
+                    remainingTime = coolTime;
+                    // test code
+                    fire();
+                }
+            }
+//        System.out.println("remainingTime : " + remainingTime);
+    }
+
     protected void fixDstRect() {
         float half_width = width / 2;
         dstRect.set(x - half_width, y - height, x + half_width, y);
