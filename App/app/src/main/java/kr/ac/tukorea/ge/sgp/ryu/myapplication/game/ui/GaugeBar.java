9e537dc0dd2b0aa3b8d4c993082db9215954d046
RYUYW <yyw980312@gmail.com>
2023-05-20 17:28:12 +0900

    HP GUI에 좌우반전 추가

--- App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/ui/GaugeBar.java
+++ App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/ui/GaugeBar.java
@@ -8,21 +8,31 @@ import kr.ac.tukorea.ge.sgp.ryu.myapplication.framework.objects.Sprite;
 
 public class GaugeBar extends Sprite{
     private final float ratio;
+    private boolean isRight;
     private Rect srcRect = new Rect();
 
-    public GaugeBar(int bitmapResId, float cx, float cy, float width, float height, float ratio){
+    public GaugeBar(int bitmapResId, float cx, float cy, float width, float height, float ratio, boolean is_right){
         super(bitmapResId, cx, cy, width, height);
         this.ratio = ratio;
+        this.isRight = is_right;
     }
     @Override
     protected void setSize(float width, float height) {
-        dstRect.set(x, y, x + width * ratio, y + height * ratio);
+        if(isRight){
+            dstRect.set(x - width * ratio, y, x, y + height * ratio);
+        }else {
+            dstRect.set(x, y, x + width * ratio, y + height * ratio);
+        }
     }
 
     public void update(float ratio) {
-        srcRect.set(0, 0, Math.round(width * ratio), Math.round(height));
-//        System.out.println("(width * ratio) " + (ratio));
-        dstRect.set(x, y, x + width * this.ratio * ratio, y + height * this.ratio);
+        if(isRight){
+            srcRect.set(Math.round(width * (1 - ratio)), 0, (int) width, Math.round(height));
+            dstRect.set(x - width * this.ratio * ratio, y, x, y + height * this.ratio);
+        }else {
+            srcRect.set(0, 0, Math.round(width * ratio), Math.round(height));
+            dstRect.set(x, y, x + width * this.ratio * ratio, y + height * this.ratio);
+        }
     }
 
     @Override
--- App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/ui/HpBar.java
+++ App/app/src/main/java/kr/ac/tukorea/ge/sgp/ryu/myapplication/game/ui/HpBar.java
@@ -4,37 +4,69 @@ import android.graphics.Canvas;
 
 import kr.ac.tukorea.ge.sgp.ryu.myapplication.R;
 import kr.ac.tukorea.ge.sgp.ryu.myapplication.framework.objects.Sprite;
+import kr.ac.tukorea.ge.sgp.ryu.myapplication.framework.view.Metrics;
 import kr.ac.tukorea.ge.sgp.ryu.myapplication.game.ship.Ship;
 
 public class HpBar extends Sprite{
-    private Ship player;
-    private static final float RATIO = 25;
-    GaugeBar hull = new GaugeBar(R.mipmap.hp, 11 * RATIO, 0, 359, 65, RATIO);
-    GaugeBar shield = new GaugeBar(R.mipmap.shield, 11 * RATIO, 0, 359, 65, RATIO);
+    private boolean isRight;
+    private Ship ship;
+    private static final float RATIO = 20;
+    GaugeBar hull;
+    GaugeBar shield;
 
-    public HpBar(Ship player){
+    public HpBar(Ship ship, boolean is_right){
         super(R.mipmap.hp_bar, 0, 0, 385 * RATIO, 65 * RATIO);
+        this.isRight = is_right;
+        float xPos;
+        int nullBitmapResId;
+        int shieldBitmapResId;
 
-        this.player = player;
+        if(is_right){
+            setBitmapResource(R.mipmap.hp_bar_right);
+            xPos = Metrics.game_width - 11 * RATIO;
+            nullBitmapResId = R.mipmap.hp_right;
+            shieldBitmapResId = R.mipmap.shield_right;
+            setPos(Metrics.game_width, 0);
+        }else {
+            xPos = 11 * RATIO;
+            nullBitmapResId = R.mipmap.hp;
+            shieldBitmapResId = R.mipmap.shield;
+        }
+        hull = new GaugeBar(nullBitmapResId, xPos, 0, 359, 65, RATIO, isRight);
+        shield = new GaugeBar(shieldBitmapResId, xPos, 0, 359, 65, RATIO, isRight);
+
+        this.ship = ship;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
     }
 
     @Override
     protected void setSize(float width, float height) {
-        dstRect.set(x, y, x + width, y + height);
+        if(isRight){
+            dstRect.set(x - width, y, x, y + height);
+        }else{
+            dstRect.set(x, y, x + width, y + height);
+        }
     }
 
     @Override
     public void update() {
-        float hullRatio = player.getOwnHP().getHullRatio();
-        float shieldRatio = player.getOwnHP().getShieldRatio();
-        hull.update(hullRatio);
-        shield.update(shieldRatio);
+        if(ship != null){
+            float hullRatio = ship.getOwnHP().getHullRatio();
+            float shieldRatio = ship.getOwnHP().getShieldRatio();
+            hull.update(hullRatio);
+            shield.update(shieldRatio);
+        }
     }
 
     @Override
     public void draw(Canvas canvas) {
-        super.draw(canvas);
-        hull.draw(canvas);
-        shield.draw(canvas);
+        if(ship != null) {
+            super.draw(canvas);
+            hull.draw(canvas);
+            shield.draw(canvas);
+        }
     }
 }
